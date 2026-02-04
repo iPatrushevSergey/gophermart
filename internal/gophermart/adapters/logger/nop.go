c150042df3830bd86ab4850cd39d81c6ae250826
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-02-04 23:34:34 +0300

    feat(adapters): add Nop logger
    Added no-op logger for tests.

---
+++ internal/gophermart/adapters/logger/nop.go
@@ -0,0 +1,17 @@
+package logger
+
+import "gophermart/internal/gophermart/application/port"
+
+// NopLogger is a no-op logger for tests.
+type NopLogger struct{}
+
+// NewNopLogger returns a logger that discards all output.
+func NewNopLogger() port.Logger {
+	return &NopLogger{}
+}
+
+func (NopLogger) Debug(msg string, args ...any) {}
+func (NopLogger) Info(msg string, args ...any)  {}
+func (NopLogger) Warn(msg string, args ...any)  {}
+func (NopLogger) Error(msg string, args ...any) {}
+func (NopLogger) Sync() error                   { return nil }
