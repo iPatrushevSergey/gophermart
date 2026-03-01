2a5ba526ac48bba3e56fb5ca35d2ac3ae3aa7127
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-03-02 00:51:18 +0300

    refactor(balance): update presentation usecase factory interface
    Updated balance presentation factory interface to current usecase contracts.

--- app/internal/gophermart/modules/balance/presentation/factory/usecase_factory.go
+++ app/internal/gophermart/modules/balance/presentation/factory/usecase_factory.go
@@ -1,9 +1,9 @@
 package factory
 
 import (
-	"gophermart/internal/gophermart/application/dto"
 	"gophermart/internal/gophermart/application/port"
-	"gophermart/internal/gophermart/domain/vo"
+	"gophermart/internal/gophermart/modules/balance/application/dto"
+	"gophermart/internal/gophermart/modules/balance/domain/vo"
 )
 
 // UseCaseFactory provides balance use cases to the presentation layer.
