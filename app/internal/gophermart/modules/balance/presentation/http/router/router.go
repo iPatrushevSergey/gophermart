52f80fea6c7740136f1549fa23d37fc1421c2510
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-03-02 19:59:06 +0300

    refactor(balance): update balance router registration
    Updated protected balance routes wiring in module router.

--- app/internal/gophermart/modules/balance/presentation/http/router/router.go
+++ app/internal/gophermart/modules/balance/presentation/http/router/router.go
@@ -4,17 +4,17 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"gophermart/internal/gophermart/application/port"
-	modulefactory "gophermart/internal/gophermart/modules/balance/presentation/factory"
-	balancehandler "gophermart/internal/gophermart/modules/balance/presentation/http/handler"
+	"gophermart/internal/gophermart/modules/balance/presentation/factory"
+	"gophermart/internal/gophermart/modules/balance/presentation/http/handler"
 )
 
 // RegisterProtectedRoutes registers protected balance endpoints.
 func RegisterProtectedRoutes(
 	protected *gin.RouterGroup,
-	useCases modulefactory.UseCaseFactory,
+	useCases factory.UseCaseFactory,
 	log port.Logger,
 ) {
-	balanceHandler := balancehandler.NewBalanceHandler(useCases, log)
+	balanceHandler := handler.NewBalanceHandler(useCases, log)
 	protected.GET("/balance", balanceHandler.Get)
 	protected.POST("/balance/withdraw", balanceHandler.Withdraw)
 	protected.GET("/withdrawals", balanceHandler.ListWithdrawals)
