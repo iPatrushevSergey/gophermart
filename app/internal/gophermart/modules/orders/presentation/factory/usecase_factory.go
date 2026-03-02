a22e1bc771f12a085786f7beef6e171b4c448700
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-03-02 20:40:16 +0300

    refactor(orders): update presentation usecase factory interface
    Updated orders presentation factory interface to current usecase contracts.

--- app/internal/gophermart/modules/orders/presentation/factory/usecase_factory.go
+++ app/internal/gophermart/modules/orders/presentation/factory/usecase_factory.go
@@ -1,13 +1,14 @@
 package factory
 
 import (
-	"gophermart/internal/gophermart/application/dto"
 	"gophermart/internal/gophermart/application/port"
-	"gophermart/internal/gophermart/domain/vo"
+	"gophermart/internal/gophermart/modules/orders/application/dto"
+	"gophermart/internal/gophermart/modules/orders/domain/vo"
 )
 
 // UseCaseFactory provides orders use cases to the presentation layer.
 type UseCaseFactory interface {
 	UploadOrderUseCase() port.UseCase[dto.UploadOrderInput, struct{}]
 	ListOrdersUseCase() port.UseCase[vo.UserID, []dto.OrderOutput]
+	ProcessAccrualUseCase() port.BackgroundRunner
 }
