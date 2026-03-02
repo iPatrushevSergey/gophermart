570dbc5907b031482b17f4a462b53d371df12461
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-03-02 20:43:59 +0300

    refactor(orders): update protected routes registration
    Updated orders router wiring for protected endpoints.

--- app/internal/gophermart/modules/orders/presentation/http/router/router.go
+++ app/internal/gophermart/modules/orders/presentation/http/router/router.go
@@ -4,17 +4,17 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"gophermart/internal/gophermart/application/port"
-	modulefactory "gophermart/internal/gophermart/modules/orders/presentation/factory"
-	ordershandler "gophermart/internal/gophermart/modules/orders/presentation/http/handler"
+	"gophermart/internal/gophermart/modules/orders/presentation/factory"
+	"gophermart/internal/gophermart/modules/orders/presentation/http/handler"
 )
 
 // RegisterProtectedRoutes registers protected orders endpoints.
 func RegisterProtectedRoutes(
 	protected *gin.RouterGroup,
-	useCases modulefactory.UseCaseFactory,
+	useCases factory.UseCaseFactory,
 	log port.Logger,
 ) {
-	orderHandler := ordershandler.NewOrderHandler(useCases, log)
+	orderHandler := handler.NewOrderHandler(useCases, log)
 	protected.POST("/orders", orderHandler.Upload)
 	protected.GET("/orders", orderHandler.List)
 }
